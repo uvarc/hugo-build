abfced384231329245b93d8c690427e27956cd28
Neal Magee <nmagee@virginia.edu>
2019-09-14 10:29:47 -0400

    Setting EXPIRES header. Has new lambda for cache refresh

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,12 @@ MAINTAINER nmagee@virginia.edu
 WORKDIR /root/
 ENV AWS_DEFAULT_REGION us-east-1
 
-# Install Git
-RUN apk update && apk add git && apk add python2-dev
+# Update, install Git and things
+RUN apk update && apk add git && apk add python2-dev && apk add py-yuicompressor
 
 # Install Hugo
-ADD https://github.com/gohugoio/hugo/releases/download/v0.40.3/hugo_0.40.3_Linux-64bit.tar.gz hugo_0.40.3.tar.gz
-RUN tar -xzf hugo_0.40.3.tar.gz
+ADD https://github.com/gohugoio/hugo/releases/download/v0.50/hugo_0.50_Linux-64bit.tar.gz hugo_0.50.tar.gz
+RUN tar -xzf hugo_0.50.tar.gz
 RUN mv hugo /usr/local/bin
 
 # Install AWSCLI
--- build-site.sh
+++ build-site.sh
@@ -6,6 +6,9 @@ git clone --depth=50 --branch=$TRAVIS_BRANCH https://github.com/uvasomrc/rc-webs
 cd rc-website
 mkdir public
 
+yuicompressor --type js static/js/scripts.js > static/js/scripts.min.js
+yuicompressor --type css static/css/style.css > static/css/style.min.css
+
 hugo -v --ignoreCache
 
 echo "-------------"
@@ -13,9 +16,14 @@ echo $TRAVIS_BRANCH
 echo "-------------"
 
 if [ $TRAVIS_BRANCH = "master" ]; then
-  aws s3 sync public s3://$BUCKET_NAME --region=us-east-1 --delete;
+  aws s3 sync public s3://$BUCKET_NAME --region=us-east-1 --cache-control public,max-age=$MAX_AGE --expires=$MAX_AGE --delete;
   aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*";
+  sleep 15;
+  aws lambda invoke --function-name web-crawl --invocation-type RequestResponse "outfile.txt"
 elif [ $TRAVIS_BRANCH = "staging" ]; then 
-  aws s3 sync public s3://$BUCKET_NAME_STAGING --region=us-east-1 --delete
-  aws cloudfront create-invalidation --distribution-id $STAGING_DISTRIBUTION_ID --paths "/*"
+  aws s3 sync public s3://$BUCKET_NAME_STAGING --region=us-east-1 --cache-control public,max-age=$MAX_AGE --expires=$MAX_AGE --delete;
+  aws cloudfront create-invalidation --distribution-id $STAGING_DISTRIBUTION_ID --paths "/*";
+elif [ $TRAVIS_BRANCH = "somrc" ]; then
+  aws s3 sync public s3://$BUCKET_NAME_SOMRC --region=us-east-1 --cache-control public,max-age=$MAX_AGE --expires=$MAX_AGE  --delete;  
+  aws cloudfront create-invalidation --distribution-id $SOMRC_DISTRIBUTION_ID --paths "/*";  
 fi
