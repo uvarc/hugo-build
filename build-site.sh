97b7804a7042b33f005fbd7cd2351bdc10971dcc
Neal Magee <nmagee@virginia.edu>
2019-11-06 12:21:56 -0500

    Trying with caching enabled

--- build-site.sh
+++ build-site.sh
@@ -9,7 +9,9 @@ mkdir public
 yuicompressor --type js static/js/scripts.js > static/js/scripts.min.js
 yuicompressor --type css static/css/style.css > static/css/style.min.css
 
-hugo -v --ignoreCache
+# Which is faster?
+hugo -v
+# hugo -v --ignoreCache
 
 echo "-------------"
 echo $TRAVIS_BRANCH
