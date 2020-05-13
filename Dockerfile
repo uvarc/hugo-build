95c099cc8243a24a4eb8da7f65c4cb2f7dd8f5cb
Neal Magee <nem2p@virginia.edu>
2020-05-13 09:06:27 -0400

    Updating and tracking. UVASOMRC alias no longer valid

--- Dockerfile
+++ Dockerfile
@@ -9,9 +9,13 @@ RUN apk update && apk add git && apk add python2-dev && apk add py-yuicompressor
 
 # Install Hugo
 ADD https://github.com/gohugoio/hugo/releases/download/v0.59.1/hugo_0.59.1_Linux-64bit.tar.gz hugo.tar.gz
-
 RUN tar -xzf hugo.tar.gz
 RUN mv hugo /usr/local/bin
+
+# Intall future version of Hugo
+ADD https://github.com/gohugoio/hugo/releases/download/v0.69.0/hugo_0.69.0_Linux-64bit.tar.gz hugo-0.69.tar.gz
+RUN tar -xzf hugo-0.69.tar.gz
+RUN mv hugo /usr/local/bin/hugo-0.69
 
 # Install AWSCLI
 ADD https://s3.amazonaws.com/aws-cli/awscli-bundle.zip awscli-bundle.zip
--- build-site.sh
+++ build-site.sh
@@ -2,15 +2,12 @@
 
 set -e
 
-git clone --depth=50 --branch=$TRAVIS_BRANCH https://github.com/uvasomrc/rc-website.git
+git clone --depth=50 --branch=$TRAVIS_BRANCH https://github.com/uvarc/rc-website.git
 cd rc-website
 mkdir public
 
 yuicompressor --type js static/js/scripts.js > static/js/scripts.min.js
 yuicompressor --type css static/css/style.css > static/css/style.min.css
-
-# Which is faster?
-hugo -v --ignoreCache
 
 echo "-------------"
 echo $TRAVIS_BRANCH
@@ -18,14 +15,17 @@ echo "-------------"
 
 EXPIRES=`date '+%a, %d %b %Y %H:%M:%S GMT' -d "+1 day"`
 echo "EXPIRES header set to: " $EXPIRES
-HUGO="hugo-v0.59"
 
 if [ $TRAVIS_BRANCH = "master" ]; then
+  HUGO="hugo-v0.59"
+  hugo -v --ignoreCache
   aws s3 sync public s3://$BUCKET_NAME --region=us-east-1 --cache-control public,max-age=$MAX_AGE --expires="$EXPIRES" --metadata generator=$HUGO --delete;
   aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*";
   sleep 15;
   aws lambda invoke --function-name web-crawl --invocation-type RequestResponse "outfile.txt"
 elif [ $TRAVIS_BRANCH = "staging" ]; then 
+  HUGO="hugo-v0.59"
+  hugo -v --ignoreCache
   aws s3 sync public s3://$BUCKET_NAME_STAGING --region=us-east-1 --cache-control public,max-age=$MAX_AGE --expires="$EXPIRES" --metadata generator=$HUGO --delete;
   aws cloudfront create-invalidation --distribution-id $STAGING_DISTRIBUTION_ID --paths "/*";
 fi
