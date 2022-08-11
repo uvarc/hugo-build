2019fd676d23b44ca2967fe28fc361980a14912f
Neal Magee <nem2p@virginia.edu>
2022-08-11 11:05:19 -0400

    Revert some minification in the build

--- build-site.sh
+++ build-site.sh
@@ -11,15 +11,15 @@ git clone --depth=50 --branch=$BRANCH https://github.com/${REPO}.git
 cd ${REPODIR}
 mkdir public
 
-# if [ -f static/js/scripts.js ]; then
-#   yuicompressor --type js static/js/scripts.js > static/js/scripts.min.js
-# fi
-# if [ -f static/css/style.css ]; then
-#   yuicompressor --type css static/css/style.css > static/css/style.min.css
-# fi
-# if [ -f static/js/user-session.js ]; then
-#   yuicompressor --type js static/js/user-session.js > static/js/user-session.min.js
-# fi
+if [ -f static/js/scripts.js ]; then
+  yuicompressor --type js static/js/scripts.js > static/js/scripts.min.js
+fi
+if [ -f static/css/style.css ]; then
+  yuicompressor --type css static/css/style.css > static/css/style.min.css
+fi
+if [ -f static/js/user-session.js ]; then
+  yuicompressor --type js static/js/user-session.js > static/js/user-session.min.js
+fi
 
 echo "-------------"
 echo $REPO
@@ -32,10 +32,10 @@ echo "EXPIRES header set to: " $EXPIRES
 if [ $BRANCH = "main" ]; then
   echo "Publishing with: " $HUGO;
   $HUGO -v --ignoreCache;
-  # /usr/bin/html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype public/index.html -o public/index.html;
-  # if [ -f public/project/index.html ]; then
-  #     /usr/bin/html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype public/project/index.html -o public/project/index.html;
-  # fi;
+  /usr/bin/html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype public/index.html -o public/index.html;
+  if [ -f public/project/index.html ]; then
+      /usr/bin/html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype public/project/index.html -o public/project/index.html;
+  fi;
   aws s3 sync public s3://$BUCKET_NAME --region=us-east-1 --cache-control public,max-age=$MAX_AGE,s-maxage=$MAX_AGE --expires="$EXPIRES" --metadata generator=$HUGO --delete;
   aws s3 cp public s3://$BUCKET_NAME --metadata-directive REPLACE --exclude "*" --include "*.jpg" --include "*.gif" --include "*.png" --recursive --cache-control max-age=604800,s-maxage=604800
   aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*";
