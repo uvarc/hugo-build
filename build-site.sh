fcbea34ae0002f362726644b19df427cd3cd1791
Diana Lin <lindiana1206@gmail.com>
2025-07-11 17:29:57 -0400

    Revert "modify build-site.sh to build /site directory"
    This reverts commit 2da7a452b5f921b7632b4c71bd76a9272b996fc3.

--- build-site.sh
+++ build-site.sh
@@ -3,8 +3,13 @@
 set -e
 
 HUGO=hugo
+# HUGO.0.81.0=${2:-hugo-0.81.0-ext}
 
-cd /site
+REPO=${1:-uvarc/rc-website}
+REPODIR=${REPO##*/}
+
+git clone --depth=50 --branch=$BRANCH https://github.com/${REPO}.git
+cd ${REPODIR}
 mkdir public
 
 if [ -f static/js/scripts.js ]; then
@@ -16,6 +21,11 @@ fi
 if [ -f static/js/user-session.js ]; then
   yuicompressor --type js static/js/user-session.js > static/js/user-session.min.js
 fi
+
+echo "-------------"
+echo $REPO
+echo $BRANCH
+echo "-------------"
 
 EXPIRES=`date '+%a, %d %b %Y %H:%M:%S GMT' -d "+1 day"`
 echo "EXPIRES header set to: " $EXPIRES
